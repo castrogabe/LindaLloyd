fd8eba6d64594134a2f411ca445908fb2204c3eb
Gabriel Castro <43317756+castrogabe@users.noreply.github.com>
2025-02-24 16:06:06 -0800

    16 Commit-Converted BACKEND from ES6 to CommonJS, EmailList, Slugify

---
+++ backend/config1.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  JWT_SECRET: process.env.JWT_SECRET || 'Objetsdart',
+  MONGODB_URL: process.env.MONGODB_URI || 'mongodb://localhost/frontend',
+
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+
+  STRIPE_PUBLISHABLE_KEY:
+    process.env.STRIPE_PUBLISHABLE_KEY || 'your_stripe_publishable_key',
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY || 'your_stripe_secret_key',
+
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || 'name',
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || 'apiKey',
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || 'secretAccessKey',
+  CLOUDINARY_URL: process.env.CLOUDINARY_URL || 'secretKey',
+
+  auth: process.env.NODE_USER,
+  auth: process.env.NODE_PASSWORD,
+};
